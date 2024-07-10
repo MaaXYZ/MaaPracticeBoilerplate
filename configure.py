0d4143b4f6f6b999e851b3342a11da2211518aef
Philos <i@philosyang.com>
2024-07-09 19:15:14 -0700

    declaring same paths as `ocr_dir`

--- configure.py
+++ configure.py
@@ -11,10 +11,11 @@ def configure_ocr_model():
         print("请完整克隆本仓库，不要漏掉 \"--recursive\"，也不要下载 zip 包！")
         exit(1)
 
-    if not (assets_dir / "resource" / "base" / "model" / "ocr").exists():   # copy default OCR model only if dir does not exist
+    ocr_dir = assets_dir / "resource" / "base" / "model" / "ocr"
+    if not ocr_dir.exists():   # copy default OCR model only if dir does not exist
         shutil.copytree(
             assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
-            assets_dir / "resource" / "base" / "model" / "ocr",
+            ocr_dir,
             dirs_exist_ok=True,
         )
     else:
