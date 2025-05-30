5dedae22cf638f3edd81ea4d09a33dde1f847e53
MistEO <mistereo@hotmail.com>
2025-05-31 02:42:29 +0800

    feat: use ppocr_v5 model

--- configure.py
+++ configure.py
@@ -14,7 +14,7 @@ def configure_ocr_model():
     ocr_dir = assets_dir / "resource" / "model" / "ocr"
     if not ocr_dir.exists():   # copy default OCR model only if dir does not exist
         shutil.copytree(
-            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
             ocr_dir,
             dirs_exist_ok=True,
         )
