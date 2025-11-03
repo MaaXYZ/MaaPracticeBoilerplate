488dcca6547baf2d2a5d03312b399075a2472db6
Natsuu <natsukawa247@outlook.com>
2025-11-03 16:11:36 +0000

    ci: 检查资源是否正常

---
+++ validate.py
@@ -0,0 +1,15 @@
+from maa.resource import Resource
+
+def validate_resource():
+    print("尝试加载资源")
+    status = Resource().post_bundle(install_path / "resource" / "base").wait().succeeded
+    if not status:
+        print("Resource validation failed.")
+        print("资源校验失败。")
+        sys.exit(1)
+    
+    print("Resource validation succeeded.")
+    print("资源校验成功。")
+
+if __name__ == "__main__":
+    validate_resource()
